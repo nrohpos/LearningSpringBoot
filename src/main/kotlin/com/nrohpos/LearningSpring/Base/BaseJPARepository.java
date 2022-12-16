64503f7450d5d7b23a766bee8baac497b2704b5b
Papaya <sophorntitheang@gmail.com>
2022-12-16 14:12:52 +0700

    remove java class

--- src/main/kotlin/com/nrohpos/LearningSpring/Base/BaseJPARepository.java
+++
@@ -1,9 +0,0 @@
-package com.nrohpos.LearningSpring.Base;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BaseJPARepository<BaseModel, Long> extends JpaRepository<BaseModel, Long> {
-
-}
--- src/main/kotlin/com/nrohpos/LearningSpring/Category/Category.java
+++
@@ -1,6 +0,0 @@
-package com.nrohpos.LearningSpring.Category;
-
-import com.nrohpos.LearningSpring.Base.BaseModel;
-
-public class Category  extends BaseModel {
-}
